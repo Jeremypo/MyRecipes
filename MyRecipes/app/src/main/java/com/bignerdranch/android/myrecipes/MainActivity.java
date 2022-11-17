1c853f1c32f5ba6377a3565feec254ffdfc415d2
Jeremypo <jeremypo4k@gmail.com>
2022-11-17 15:15:21 -0800

    Added MainMenu Functions
    To-Do:
    - Fix View Recipe(s) View
    - Add delete recipe functionality
    - Add Grocery list generator
    
    Completed:
    + Made View Recipe(s) button and Grocery List button un-clickable if no recipes saved (currently > 2).

--- MyRecipes/app/src/main/java/com/bignerdranch/android/myrecipes/MainActivity.java
+++ MyRecipes/app/src/main/java/com/bignerdranch/android/myrecipes/MainActivity.java
@@ -7,11 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    private HashMap<String, ArrayList<IngredientModel>> saveData = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         configureCreateRecipeButton();
+        configureViewRecipeButton();
+        configureGroceryListButton();
+    }
+
+    @Override
+    protected void onResume(){
+        configureViewRecipeButton();
+        configureGroceryListButton();
+        super.onResume();
     }
 
     private void configureCreateRecipeButton() {
@@ -29,6 +42,65 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, CreateRecipeActivity.class));
             }
         });
+    }
+
+    private void configureViewRecipeButton() {
+        Button viewRecipeButton = (Button) findViewById(R.id.view_main);
+        viewRecipeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //startActivity(new Intent(MainActivity.this, CreateRecipeActivity.class));
+                System.out.println(saveData.size());
+            }
+        });
+        if(noRecipes()){
+            viewRecipeButton.setAlpha((float)0.25);
+            viewRecipeButton.setClickable(false);
+        }
+        else{
+            viewRecipeButton.setAlpha(1);
+            viewRecipeButton.setClickable(true);
+        }
+    }
+
+    private void configureGroceryListButton(){
+        Button viewRecipeButton = (Button) findViewById(R.id.grocery_main);
+        viewRecipeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //startActivity(new Intent(MainActivity.this, CreateRecipeActivity.class));
+                System.out.println(saveData.size());
+            }
+        });
+        if(noRecipes()){
+            viewRecipeButton.setAlpha((float)0.25);
+            viewRecipeButton.setClickable(false);
+        }
+        else{
+            viewRecipeButton.setAlpha(1);
+            viewRecipeButton.setClickable(true);
+        }
+    }
+
+    private boolean noRecipes(){
+        boolean empty = true;
+
+        File file = new File(getDir("data", MODE_PRIVATE), "recipes");
+        if(!file.exists())
+            System.out.println("file DNE");
+        else {
+            ObjectInputStream inputStream = null;
+            try {
+                inputStream = new ObjectInputStream(new FileInputStream(file));
+                saveData = (HashMap<String, ArrayList<IngredientModel>>)inputStream.readObject();
+                if(saveData.size() > 2)
+                    empty = false;
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return empty;
     }
 
 }
